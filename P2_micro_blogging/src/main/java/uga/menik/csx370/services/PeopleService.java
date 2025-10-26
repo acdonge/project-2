1c3a325a0693e3afcc615c3f19787347c48aa5cb
acdonge <kb083509@gmail.com>
2025-10-25 22:48:08 -0400

    Commit from  on Sat, Oct 25, 2025 10:48:07 PM

--- P2_micro_blogging/src/main/java/uga/menik/csx370/services/PeopleService.java
+++ P2_micro_blogging/src/main/java/uga/menik/csx370/services/PeopleService.java
@@ -1,107 +1,83 @@
+/**
+Copyright (c) 2024 Sami Menik, PhD. All rights reserved.
+
+This is a project developed by Dr. Menik to give the students an opportunity to apply database concepts learned in the class in a real world project. Permission is granted to host a running version of this software and to use images or videos of this work solely for the purpose of demonstrating the work to potential employers. Any form of reproduction, distribution, or transmission of the software's source code, in part or whole, without the prior written consent of the copyright owner, is strictly prohibited.
+*/
 package uga.menik.csx370.services;
 
+import java.util.List;
+import java.util.ArrayList;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
+
 import javax.sql.DataSource;
-import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+
 import uga.menik.csx370.models.FollowableUser;
+import uga.menik.csx370.utility.Utility;
 
 /**
- * Service: handles logic for the People page.
- * Shows who can be followed and manages follow/unfollow actions.
- * Uses raw JDBC for transparency and control.
+ * This service contains people related functions.
  */
 @Service
 public class PeopleService {
-
-    private final DataSource pool;
-
-    @Autowired
-    public PeopleService(DataSource pool) {
-        this.pool = pool;
+    
+    private final DataSource dataSource;
+    /**
+     * This function should query and return all users that 
+     * are followable. The list should not contain the user 
+     * with id userIdToExclude.
+     */
+    public PeopleService(DataSource dataSource) {
+        this.dataSource = dataSource;
     }
 
-    /**
-     * Retrieves a list of all users except the current one.
-     * Adds each user's most recent post timestamp for the UI display.
-     */
-    public List<FollowableUser> getFollowableUsers(String currentUserId) {
-        List<FollowableUser> results = new ArrayList<>();
+    public List<FollowableUser> getFollowableUsers(String userIdToExclude) {
+        // Write an SQL query to find the users that are not the current user.
+    
+        // Run the query with a datasource.
+        // See UserService.java to see how to inject DataSource instance and
+        // use it to run a query.
 
-        final String q =
-            "SELECT u.userId, u.username, u.firstName, u.lastName, " +
-            "  DATE_FORMAT((SELECT MAX(p.created_at) FROM post p WHERE p.userId = u.userId), " +
-            "  '%b %d, %Y, %h:%i %p') AS last_post_time " +
-            "FROM user u WHERE u.userId <> ? ORDER BY u.username ASC";
+        // Use the query result to create a list of followable users.
+        // See UserService.java to see how to access rows and their attributes
+        // from the query result.
+        // Check the following createSampleFollowableUserList function to see 
+        // how to create a list of FollowableUsers.
 
-        try (Connection c = pool.getConnection();
-             PreparedStatement ps = c.prepareStatement(q)) {
+        // Replace the following line and return the list you created.
 
-            ps.setString(1, currentUserId);
+        List<FollowableUser> followableUsers = new ArrayList<>();
+        final String sql = "select * from user where userId = <> ?";
+        
+        try (Connection conn = dataSource.getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
 
-            try (ResultSet r = ps.executeQuery()) {
-                while (r.next()) {
-                    results.add(new FollowableUser(
-                        r.getString("userId"),
-                        r.getString("username"),
-                        r.getString("firstName"),
-                        r.getString("lastName"),
-                        r.getString("last_post_time"),
-                        false // default follow state
-                    ));
+            pstmt.setString(1, userIdToExclude);
+
+            try (ResultSet rs = pstmt.executeQuery()) {
+                while (rs.next()) {
+                    FollowableUser user = new FollowableUser(
+                        rs.getString("userId"),
+                        rs.getString("firstName"),
+                        rs.getString("lastName"),
+                        rs.getBoolean("isFollowed"),
+                        rs.getString("lastActiveDate")
+                    );
+                    followableUsers.add(user);
                 }
             }
 
-        } catch (SQLException sqle) {
-            System.err.println("[PeopleService] DB error while loading followable users:");
-            sqle.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
-
-        return results;
+        
+        return followableUsers;
+        
     }
 
-    /**
-     * Inserts a follow relation. Duplicate entries are ignored.
-     */
-    public void followUser(String followerId, String followeeId) {
-        final String add =
-            "INSERT IGNORE INTO follows (follower_id, followee_id, created_at) VALUES (?, ?, NOW())";
-
-        try (Connection c = pool.getConnection();
-             PreparedStatement ps = c.prepareStatement(add)) {
-
-            ps.setString(1, followerId);
-            ps.setString(2, followeeId);
-            ps.executeUpdate();
-
-        } catch (SQLException sqle) {
-            System.err.println("[PeopleService] DB error while adding follow:");
-            sqle.printStackTrace();
-        }
-    }
-
-    /**
-     * Deletes an existing follow link.
-     */
-    public void unfollowUser(String followerId, String followeeId) {
-        final String del =
-            "DELETE FROM follows WHERE follower_id = ? AND followee_id = ?";
-
-        try (Connection c = pool.getConnection();
-             PreparedStatement ps = c.prepareStatement(del)) {
-
-            ps.setString(1, followerId);
-            ps.setString(2, followeeId);
-            ps.executeUpdate();
-
-        } catch (SQLException sqle) {
-            System.err.println("[PeopleService] DB error while removing follow:");
-            sqle.printStackTrace();
-        }
-    }
 }
